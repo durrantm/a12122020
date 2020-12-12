6c95d18ca07d98f2fb551fa481185d45eb843dfc
Mohit Yadav <71254120+mohit21dh@users.noreply.github.com>
2020-12-12 22:57:44 +0400

    Update showDayName.js

--- showDayName.js
+++ showDayName.js
@@ -1,12 +1,13 @@
 const showDayName = (apiDate) => {
-  const utcDate = new Date(apiDate);
-  const options = {
-    weekday: 'long',
-    timeZone: 'UTC'
-  };
-  const localDateString = new Intl.DateTimeFormat('en-US', options).format(utcDate);
+  const c = new Date(`${apiDate}Z`.replace(/\s+/g, 'T'));
+  const timeOffset = new Date().getTimezoneOffset();
+  const millisecondOffset = timeOffset*60*1000;
+  // add time for all browser compatibility for having uniform UTC
+  const utcDate = new Date(c.getTime() + millisecondOffset);
+  // now subtract offset time for getting local time
+  const localDateString = String(new Date(utcDate.getTime() - millisecondOffset) );
   const day = localDateString.substring(0,3);
   return day;
-};
+}
 showDayName('2020-12-25T02:00:00');
 showDayName('2020-12-25T23:00:00');
